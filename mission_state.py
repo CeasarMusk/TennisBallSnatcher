74c7f01a22d17ba19894c7d55f728ce3c1bb88ee
alexander-leon5 <alexanderl.leon5@gmail.com>
2026-03-26 18:58:04 -0400

    Update mission_state.py

--- mission_state.py
+++ mission_state.py
@@ -3,15 +3,18 @@ class DroneStateMachine:
     def __init__(self, fc):
         self.fc = fc
         self.state = "IDLE"
+        print("Idle state\n")
 
     def update(self):
         if self.state == "IDLE":
             self.fc.set_guided_mode()
+            print("guided state\n")
             self.fc.arm()
             self.state = "TAKEOFF"
 
         elif self.state == "TAKEOFF":
             if not hasattr(self, "takeoff_sent"):
+                print("attempting takeoff\n")
                 self.fc.takeoff(3)
                 self.takeoff_sent = True
 
