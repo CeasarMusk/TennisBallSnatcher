c6774c7f3aedd72af29e618415b53c54c37161f5
alexander-leon5 <alexanderl.leon5@gmail.com>
2026-03-26 18:10:09 -0400

    Created mission_state.py
    This is the brains of the drone, it decides which "mission state" the drone is in. a.k.a what the drones current goal is

---
+++ mission_state.py
@@ -0,0 +1,12 @@
+class DroneStateMachine:
+  def __init__(self, fc):
+    self.fc = fc
+    self.state = "IDLE"
+  def update(self):
+    if self.state == "IDLE":
+      self.fc.arm()
+      self.state = "TAKEOFF"
+
+    elif self.state == "TAKEOFF":
+      self.fc.takeoff(3)
+      self.state = "NAVIGATE"
