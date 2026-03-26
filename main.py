4549aa0e21865d61e643a4c3db133dae5476f404
alexander-leon5 <alexanderl.leon5@gmail.com>
2026-03-26 18:22:48 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from pymavlink import mavutil
+from flightcontroller_state import FlightController
+from mission_state import DroneStateMachine
+
+master = mavutil.mavlink_connection('/dev/ttyACM0', baud=921600)
+master.wait_heartbeat()
+
+fc = FlightController(master)
+sm = DroneStateMachine(fc)
+
+while True:
+    sm.update()
