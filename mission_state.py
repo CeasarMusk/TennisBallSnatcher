95819da4e547b227cdf3ec35178a1c0b6e958962
alexander-leon5 <alexanderl.leon5@gmail.com>
2026-03-27 15:16:06 -0400

    Update mission_state.py

--- mission_state.py
+++ mission_state.py
@@ -1,7 +1,5 @@
 class DroneStateMachine:
 
-    takeoff_height = 1
-
     def __init__(self, fc):
         self.fc = fc
         self.state = "IDLE"
@@ -18,10 +16,10 @@ class DroneStateMachine:
         elif self.state == "TAKEOFF":
             if not hasattr(self, "takeoff_sent"):
                 print("attempting takeoff\n")
-                self.fc.takeoff(takeoff_height)
+                self.fc.takeoff(1)
                 self.takeoff_sent = True
 
             alt = self.fc.get_altitude()
-            if alt is not None and alt >= takeoff_height:
+            if alt is not None and alt >= 1:
                 self.state = "NAVIGATE"
                 print("Takeoff success")
