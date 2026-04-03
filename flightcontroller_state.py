a13c8d8c5ccfcc6775e758531b0c43a3433d40bc
Kyroy123 <kiroyousef4@gmail.com>
2026-04-03 14:01:02 -0400

    Update flightcontroller_state.py

--- flightcontroller_state.py
+++ flightcontroller_state.py
@@ -18,6 +18,24 @@ class FlightController:
         print("attemping to arm")
         self.master.motors_armed_wait()
         print("armed")
+    def disarm(self):
+        print("Attempting to disarm...")
+        self.master.arducopt
+    # Force the motors to stop immediately
+    # 0 = disarm, 1 = arm
+    # 21196 is the "force" parameter to ensure it happens even if not in a safe state
+        self.master.mav.command_long_send(
+        self.master.target_system,
+        self.master.target_component,
+        193, # MAV_CMD_COMPONENT_ARM_DISARM
+        0,
+        0, 0, 0, 0, 0, 0, 0 # Parameters
+        )
+    # Wait for the confirmation that motors have stopped
+        self.master.motors_disarmed_wait()
+        print("Disarmed successfully")
+
+    
 
     def takeoff(self, altitude):
         self.master.mav.command_long_send(
